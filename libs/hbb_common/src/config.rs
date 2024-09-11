fff142648624234da3f95d884e9326c60a35065e
ranran0708 <85716337+ranran0708@users.noreply.github.com>
2024-09-11 17:20:51 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.4.10"];
-pub const PUBLIC_RS_PUB_KEY: &str = "HamZg77DvWYs8JC1v6sAa2nSKRoGaXTGgkdtSZYfV1Q=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.yuexinggy.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "HamZg77DvWYs8JC1v6sAa2nSKRoGaXTGgkdtSZYfV1Q";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
